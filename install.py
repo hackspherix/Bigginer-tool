3ad7fd37197d2978e2c34830057894418a926eb3
hackspherix <hackspherix@gmail.com>
2025-08-29 07:47:30 +0530

    install.py

---
+++ install.py
@@ -0,0 +1,15 @@
+# Basic Ethical Hacker Beginner Tool
+# Author: AFILPM
+
+def main():
+    print("====================================")
+    print(" 🔐 Ethical Hacking Beginner Tool 🔐 ")
+    print("====================================")
+    print("This is your first Python tool!")
+    print("You ran it successfully 🎉")
+    print("------------------------------------")
+    print("Use Python to build more tools step by step.")
+    print("====================================")
+
+if __name__ == "__main__":
+    main()
